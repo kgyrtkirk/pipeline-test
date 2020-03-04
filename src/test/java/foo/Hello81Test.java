db1309ebbae9b53deb0ec14348bef10ce60c4ef1
Zoltan Haindrich <kirk@rxd.hu>
2020-03-04 22:10:06 +0100

    add gen/etc

---
+++ src/test/java/foo/Hello81Test.java
@@ -0,0 +1,23 @@
+package foo;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Hello81Test {
+    private static final int MULTIPLIER;
+    static {
+        String multiplier = System.getenv("MULTIPLIER");
+        MULTIPLIER = multiplier != null ? Integer.parseInt(multiplier) : 1;
+    }
+    @Test public void one() {
+        if (Math.random() < 0.015) {
+            fail("oops");
+        }
+    }
+    @Test public void two() {}
+    @Test public void three() throws Exception {
+        Thread.sleep(810 * MULTIPLIER);
+    }
+    @Test public void four() throws Exception {
+        Thread.sleep(1000 * MULTIPLIER);
+    }
+}
