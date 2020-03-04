30df6ec1dd56f0a1253cac968dfc40199cc471bf
Zoltan Haindrich <kirk@rxd.hu>
2020-03-04 22:02:32 +0100

    add testInParallel

---
+++ testInParallel.groovy
@@ -0,0 +1,29 @@
+def call(parallelism, inclusionsFile, exclusionsFile, results, image, prepare, run) {
+  def splits
+  node {
+    prepare()
+    splits = splitTests parallelism: parallelism, generateInclusions: true, estimateTestsFromFiles: true
+  }
+  def branches = [:]
+  for (int i = 0; i < splits.size(); i++) {
+    def num = i
+    def split = splits[num]
+    branches["split${num}"] = {
+      stage("Test Section #${num + 1}") {
+        docker.image(image).inside {
+          stage('Preparation') {
+            prepare()
+            writeFile file: (split.includes ? inclusionsFile : exclusionsFile), text: split.list.join("\n")
+            writeFile file: (split.includes ? exclusionsFile : inclusionsFile), text: ''
+          }
+          stage('Main') {
+            realtimeJUnit(results) {
+              run()
+            }
+          }
+        }
+      }
+    }
+  }
+  parallel branches
+}
